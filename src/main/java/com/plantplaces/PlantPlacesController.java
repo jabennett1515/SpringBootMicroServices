67e9377d0eaf117a32fbc09c253a35799844d04b
Administrator <Administrator@DESKTOP-099UQRG>
2020-09-17 14:38:58 -0400

    update /endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,6 +3,12 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Administrator
+ * Handle the /start endpoint
+ * @return
+ */
+
 @Controller
 public class PlantPlacesController {
 	
@@ -11,6 +17,12 @@ public class PlantPlacesController {
 		return "start";
 	}
 	
+	
+	/**
+	 * 
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "start";
